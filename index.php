0715c314509e15d0b40d44437947136f0295cda6
Hamsa Mohamed <hamsa230103449@sut.edu.eg>
2025-03-04 21:54:06 +0200

    Add files via upload
    Assignment1 Web-Advanced

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+
+/*Ex:1
+$x = 10;
+$y = 7;
+
+echo "10 + 7 = " . ($x + $y) . "<br>";
+echo "10 - 7 = " . ($x - $y) . "<br>";
+echo "10 * 7 = " . ($x * $y) . "<br>";
+echo "10 / 7 = " . ($x / $y) . "<br>";
+echo "10 % 7 = " . ($x % $y) . "<br>";
+
+
+
+
+
+EX:2
+$month = date('F', time()); 
+
+if ($month == "August") {
+    echo "It's August, so it's really hot.";
+} else {
+    echo "Not August, so at least not in the peak of the heat.";
+}
+
+
+EX:3
+for ($i = 1; $i <= 12; $i++) {
+    echo "$i * $i = " . ($i * $i) . "<br>";
+}
+
+Ex:4
+
+echo "<table border='1' cellspacing='0' cellpadding='5'>";
+echo "<tr><td></td>"; 
+
+for ($i = 1; $i <= 7; $i++) {
+    echo "<td><b>$i</b></td>";
+}
+echo "</tr>";
+
+for ($i = 1; $i <= 7; $i++) {
+    echo "<tr><td><b>$i</b></td>"; 
+    for ($j = 1; $j <= 7; $j++) {
+        echo "<td>" . ($i * $j) . "</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+
+
+
+*/
+
+
+?>
